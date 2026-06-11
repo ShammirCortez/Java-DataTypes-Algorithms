472b9cd3ca8ba886cae9c136a59e2f4b3e233627
Shammir Cortez <cortezshammir@gmail.com>
2026-06-11 09:15:04 +0100

    First datatype problem solved, reversing a linked list (lol classic)

---
+++ JavaPuzzles/src/listNodes/ReverseLinkedList.java
@@ -0,0 +1,51 @@
+package listNodes;
+import dataTypes.ListNode;
+
+public class ReverseLinkedList {
+
+	public static void main(String[] args) {
+		
+		ListNode listNodeCreation = new ListNode();
+		ListNode toReverse = listNodeCreation;
+		
+		listNodeCreation.next = new ListNode(1);
+		listNodeCreation = listNodeCreation.next;
+		
+		listNodeCreation.next = new ListNode(2);
+		listNodeCreation = listNodeCreation.next;
+		
+		listNodeCreation.next = new ListNode(3);
+		listNodeCreation = listNodeCreation.next;
+		
+		listNodeCreation.next = new ListNode(4);
+		listNodeCreation = listNodeCreation.next;
+		
+		listNodeCreation.next = new ListNode(5);
+		listNodeCreation = listNodeCreation.next;
+		
+		System.out.println(ListNode.print(toReverse));
+		
+		ListNode reversedNode = reverseNodes(toReverse);
+		
+		System.out.println(ListNode.print(reversedNode));
+
+	}
+	
+	public static ListNode reverseNodes(ListNode list) {
+		
+		ListNode bump = null;
+		
+		while (list != null) {
+			
+			ListNode second = list.next;
+			list.next = bump;
+			bump = list;
+			list = second;
+			
+		}
+		
+		return bump;
+		
+	}
+	
+}
