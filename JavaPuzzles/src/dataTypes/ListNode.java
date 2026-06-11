835c5ab1dd1b397147f8415cb4c944267af286c7
Shammir Cortez <cortezshammir@gmail.com>
2026-06-11 09:07:56 +0100

    Creation and addition of ListNode datatype

---
+++ JavaPuzzles/src/dataTypes/ListNode.java
@@ -0,0 +1,36 @@
+package dataTypes;
+import java.lang.StringBuilder;
+
+public class ListNode {
+	
+	public int val;
+	public ListNode next;
+	
+	public ListNode(){}
+	
+	public ListNode(int val){
+		this.val = val;
+	}
+	
+	public ListNode(int val, ListNode next){
+		this.val = val;
+		this.next = next;
+	}
+	
+	public static String print(ListNode temp) {
+		
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("( ");
+		
+		while (temp.next != null) {
+			builder.append("" + temp.val + ", ");
+			temp = temp.next;
+		}
+		
+		builder.append(" )");
+		
+		return builder.toString();
+	}
+	
+}
